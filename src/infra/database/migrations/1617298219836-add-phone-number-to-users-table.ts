b36ba9b5e40ea0cc72d723d31863a798ea3308e3
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2021-04-08 11:26:15 -0300

    Fix migrations code formatting

--- src/infra/database/migrations/1617298219836-add-phone-number-to-users-table.ts
+++ src/infra/database/migrations/1617298219836-add-phone-number-to-users-table.ts
@@ -1,14 +1,16 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export class addPhoneNumberToUsersTable1617298219836 implements MigrationInterface {
-    name = 'addPhoneNumberToUsersTable1617298219836'
+export class addPhoneNumberToUsersTable1617298219836
+implements MigrationInterface {
+  name = 'addPhoneNumberToUsersTable1617298219836'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ADD "phone_number" character varying NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'ALTER TABLE "users" ADD "phone_number" character varying NOT NULL'
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "phone_number"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE "users" DROP COLUMN "phone_number"')
+  }
 }
